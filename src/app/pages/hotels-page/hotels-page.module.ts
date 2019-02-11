832e44f0bcc4b355038233fc06dd548be0ba7df7
Inessa Borodich <iborodich@gds000387-nb.carina.griddynamics.net>
2019-02-11 16:44:22 +0400

    Add hotels page form validation

--- src/app/pages/hotels-page/hotels-page.module.ts
+++ src/app/pages/hotels-page/hotels-page.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule, MatCardModule,
   MatDatepickerModule,
@@ -30,7 +30,6 @@ import { HotelsPageComponent } from './hotels-page.component';
     MatIconModule,
     MatOptionModule,
     MatFormFieldModule,
-    FormsModule,
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
